b5aefe43df338be75724cc02a4b24e7e629eb915
bohdanhulchuknl <bohdanhulchuknl@gmail.com>
2023-07-31 17:33:48 +0300

    ver7

--- index.js
+++ index.js
@@ -1,82 +1,86 @@
-require('dotenv').config()
-const express = require('express');
-const passport = require('passport');
-const session = require('express-session');
-require('./passport');
-
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+require("./passport");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000
-
-// express session 
-app.use(session({
+const port = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://salon-gg-client.vercel.app"],
+    credentials: true,
+  })
+);
+// express session
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-  }));
+    saveUninitialized: false,
+  })
+);
 
-  
 app.use(passport.initialize());
 app.use(passport.session());
-  
+
 // Middleware used in protected routes to check if the user has been authenticated
 const isLoggedIn = (req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.sendStatus(401);
-    }
-}
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 // Base route
 app.get("/home", (req, res) => {
-    res.send("Home Page")
-})
+  res.send("Home Page");
+});
 
 // Google Auth consent screen route
-app.get('/auth/google',
-    passport.authenticate('google', {
-            scope:
-                ['email', 'profile']
-        }
-));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
 
 // Call back route
-app.get('/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/failed',
-    }),
-    function (req, res) {
-        res.redirect('/success')
-
-    }
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failed",
+  }),
+  function (req, res) {
+    res.redirect("https://salon-gg-client.vercel.app");
+  }
 );
 
 // failed route if the authentication fails
 app.get("/failed", (req, res) => {
-    console.log('User is not authenticated');
-    res.send("Failed")
-})
-
-// Success route if the authentication is successful
-app.get("/success",isLoggedIn, (req, res) => {
-    console.log('You are logged in');
-    res.send(`Welcome ${req.user.displayName}`)
-})
-
-// Route that logs out the authenticated user  
-app.get("/auth/logout", (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.log('Error while destroying session:', err);
-        } else {
-            req.logout(() => {
-                console.log('You are logged out');
-                res.redirect('/home');
-            });
-        }
-    });
+  console.log("User is not authenticated");
+  res.send("Failed");
 });
 
+// Success route if the authentication is successful
+app.get("/success", isLoggedIn, (req, res) => {
+  console.log("You are logged in");
+  res.send(req.user);
+});
 
+// Route that logs out the authenticated user
+app.get("/auth/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Error while destroying session:", err);
+    } else {
+      req.logout(() => {
+        console.log("You are logged out");
+        res.redirect("https://salon-gg-client.vercel.app");
+      });
+    }
+  });
+});
 
-app.listen(port, () => console.log("server running on port" + port))
+app.listen(port, () => console.log("server running on port" + port));
--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/auth/google/callback",
+        callbackURL: "https://salon-gg-server.vercel.app/auth/google/callback",
         passReqToCallback   : true
 },
 function(request, accessToken, refreshToken, profile, done) {
